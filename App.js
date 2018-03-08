b43e88e88f6404e88a5985d78048e99007d7f47b
Christian Wang <christian.gnaw@gmail.com>
2018-03-07 23:21:29 -0500

    Added basic grid framework

--- App.js
+++ App.js
@@ -6,22 +6,29 @@ import styles from './styles';
 export default class App extends Component {
     render() {
         return (
-            <View style={styles.row}>
-                <View style={styles.col6}>
-                <Text>Column 1</Text>
-                    <Button
-                        color='#42f4c5'
-                        title="MasterAnime"
-                        onPress={() => { Alert.alert("You Pressed Me!") }}
-                    />
+            <View style={styles.screen}>
+                <View style={styles.row}>
+                    <View style={styles.col12, styles.centerX}>
+                        <Text style={styles.h1}>Amine</Text>
                     </View>
-                     <View style={styles.col6}>
-                     <Text>Column 2</Text>
-                    <Button
-                        color='#42f4c5'
-                        title="MasterAnime"
-                        onPress={() => { Alert.alert("You Pressed Me!") }}
-                    />
+                </View>
+                <View style={styles.row}>
+                    <View style={styles.col6}>
+                        <Text>Column 1</Text>
+                        <Button
+                            color='#42f4c5'
+                            title="MasterAnime"
+                            onPress={() => { Alert.alert("You Pressed Me!") }}
+                        />
+                    </View>
+                    <View style={styles.col6}>
+                        <Text>Column 2</Text>
+                        <Button
+                            color='#42f4c5'
+                            title="MasterAnime"
+                            onPress={() => { Alert.alert("You Pressed Me!") }}
+                        />
+                    </View>
                 </View>
             </View>
         );
