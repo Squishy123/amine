8db46a8e9f7fae87e2e4c4710e67eb0b45f22a22
Christian Wang <christian.gnaw@gmail.com>
2018-03-13 00:15:03 -0400

    Added working 9anime scraper prototype

---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+import fetch from 'isomorphic-fetch';
+export default {
+  //resolve any async stuff
+  async asyncData() {
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
