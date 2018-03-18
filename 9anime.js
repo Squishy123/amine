fb0818ea595b8daf9b74b00fbcb05a213a1e7be9
Christian Wang <christian.gnaw@gmail.com>
2018-03-17 21:21:46 -0400

    Finished scraper with few issues

--- 9anime.js
+++ 9anime.js
@@ -1,10 +1,14 @@
-///Import nickjs
+///Import puppeteer
 const puppeteer = require('puppeteer');
 
+//Import json file writer
 const jsonfile = require('jsonfile');
 
 //Import util lib
 const util = require('./util');
+
+//import async lib
+const async = require('async');
 
 /**
  * Searches 9anime films by keyword
@@ -12,8 +16,8 @@ const util = require('./util');
  * @param {String} query 
  */
 async function search(page, query) {
-    page.waitForSelector('#main > div > div:nth-child(1) > div.widget-body > div.film-list', { visible: true});
-    await util.search(page, `https://www3.9anime.is/search?keyword=${query}/`, {timeout: 0});
+    page.waitForSelector('#main > div > div:nth-child(1) > div.widget-body > div.film-list', { visible: true });
+    await util.search(page, `https://www3.9anime.is/search?keyword=${query}/`, { timeout: 0, waituntil: "domcontentloaded" });
 }
 
 /**
@@ -31,8 +35,8 @@ async function getSearchResults(page, query) {
     for (let p = 1; p <= numPages; p++) {
         //go to next page
         currentURL = `https://www3.9anime.is/search?keyword=${query}/` + `&page=${p}`;
-        page.waitForSelector('#main > div > div:nth-child(1) > div.widget-body > div.film-list', { visible: true});
-        await util.search(page, currentURL, {timeout: 0});
+        page.waitForSelector('#main > div > div:nth-child(1) > div.widget-body > div.film-list', { visible: true });
+        await util.search(page, currentURL, { timeout: 0 });
 
         let listLength = await util.getNumElements(page, '.item');
 
@@ -42,7 +46,7 @@ async function getSearchResults(page, query) {
         let link = await util.grabHREF(page, `a.name`);
         //grab title
         let title = await util.grabHTML(page, `a.name`);
-        console.log(img);
+        //console.log(img);
         for (let i = 1; i <= listLength; i++) {
             let arg = { selector: `#main > div > div:nth-child(1) > div.widget-body > div.film-list > div:nth-child(${i}) > div > a.poster.tooltipstered > div` };
             //grab status
@@ -56,7 +60,7 @@ async function getSearchResults(page, query) {
 
             // console.log(link)
             if (title[i - 1] && status && img[i - 1] && link[i - 1]) {
-                console.log(`Title: ${title[i - 1]}`);
+                //console.log(`Title: ${title[i - 1]}`);
                 searchItems.push({ title: title[i - 1], status: status, img: img[i - 1], link: link[i - 1] });
             }
         }
@@ -66,12 +70,132 @@ async function getSearchResults(page, query) {
 }
 
 /**
+ * Grabs the links for episodes of a given url search result
+ * @param {String} url 
+ */
+async function getEpisodeLinks(page, url) {
+    //nav to url
+    page.waitForSelector('#player', { visible: true });
+    await util.search(page, url, { timeout: 0, waituntil: "domcontentloaded" });
+    let sources = [];
+
+    //grab providers and sort them out by name
+    let serverProviders = await page.evaluate(() => {
+        let tabs = [];
+        $('#main > div > div.widget.servers > div.widget-title > span.tabs').children().each(async (index, value) => {
+            tabs.push($(value).text());
+        });
+        return tabs;
+    });
+
+    serverProviders.forEach((e) => {
+        sources.push({ serverProvider: e });
+    });
+
+    //iterate through sources
+    for (let s = 1; s <= sources.length; s++) {
+        //grab range of eps(they usually come in 50s)
+        let range = await page.evaluate(() => {
+            let el = $('#main > div > div.widget.servers > div.widget-body > div:nth-child(1) > div');
+            return (el.length) ? el.children().length : 1;
+        });
+
+        for (let r = 1; r <= range; r++) {
+            let episodes = [];
+            let listLength = await page.evaluate(() => {
+                let el = $('#main > div > div.widget.servers > div.widget-body > div:nth-child(1) > ul');
+                return (el.length) ? el.children().length : 0;
+            });
+            console.log(listLength)
+            for (let l = 1; l <= listLength; l++) {
+                let eps = await util.grabHREF(page, `#main > div > div.widget.servers > div.widget-body > div:nth-child(${s}) > ul > li:nth-child(${l}) > a`)
+                episodes.push(`https://www3.9anime.is${eps}`);
+            }
+            if (sources[s - 1].episodes) sources[s - 1].episodes = sources[s - 1].episodes.concat(episodes);
+            else sources[s - 1].episodes = episodes;
+        }
+    }
+    return sources;
+}
+
+/**
+ * Returns the url of the video file of the given player
+ * @param {Page} page 
+ * @param {String} url 
+ */
+async function getPlayerFile(browser, page, url) {
+    let whitelist = ["aspx",
+        "axd",
+        "html",
+        "js",
+        "css",
+        "rapidvideo",
+        "mp4",
+        "video",
+        "9anime",
+        "disqus",
+        url.slice(url.length - 6, url.length)];
+    page.waitForSelector('#player', { visible: true }).then(() => {
+        console.log("Player has appeared")
+    });
+    //block popups
+    await page.evaluateOnNewDocument(() => {
+        window.open = () => null;
+    });
+
+    //stop loading network crap
+    await page.setRequestInterception(true);
+
+    page.on('request', interceptedRequest => {
+        let request = false;
+        whitelist.forEach((e) => {
+            let url = interceptedRequest.url().toString();
+            //console.log(url);
+            if (url.includes(e)) request = true;
+        })
+        if (request) interceptedRequest.continue();
+        else interceptedRequest.abort();
+    });
+    //page.setAdBlockingEnabled();
+    await util.search(page, url, { timeout: 0, "waituntil" : "domcontentloaded" }).then(() => {
+        console.log("Finished searching")
+    });
+
+    //add mixed content requests
+    await page.evaluate(() => {
+        $('head').append('<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">');
+    });
+
+    let click = await page.click('#player');
+    //await page.waitForFunction(() => {
+    //    return document.getElementById("#videojs");
+    //});
+    let n = 0;
+    async function proclink() {
+        n++;
+        let link = await page.evaluate(() => {
+            //console.log();
+            return $('#videojs_html5_api').attr('src');
+        });
+        console.log(n, " link ", link);
+        if (!link) return proclink();
+        return link;
+    }
+
+    let link = await proclink();
+    console.log("Job done")
+}
+
+/**
  * Main
  */
 async function main() {
     //setup puppeteer browser
     let browser = await puppeteer.launch({
-        headless: true
+        headless: false,
+        args: ["--disable-web-security"],
+        devtools: false,
+        executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
     });
 
     (async () => {
@@ -79,16 +203,25 @@ async function main() {
         //inject jquery so we can select stuff
         await util.injectjQuery(page);
         //search for title
-        let results = await getSearchResults(page, "dragon ball super");
-        jsonfile.writeFileSync('results.json', results);
+        //let results = await getSearchResults(page, "steins gate");
+        //jsonfile.writeFileSync('searchResults.json', results);
+
+        let cached = require('./fileLinks.json');
+        //grab episode links
+        //let links = await getEpisodeLinks(page, cached[0].link);
+        //jsonfile.writeFileSync('fileLinks.json', links);
+
+        //grab video file
+        let file = await getPlayerFile(browser, page, cached[0].episodes[0]);
+        //sonfile.writeFileSync('link.html', file);
 
     })().then(() => {
-        browser.close();
+        //browser.close();
         console.log("Finished Execution");
         //exit code
     })
         .catch((err) => {
-            browser.close();
+            //browser.close();
             //exit with error 
             console.error("Execution unsuccessful: ", err);
 
