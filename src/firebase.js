78c80fe0f4bbfdd37ba7ebc6a9dc2077e3554598
Squishy123 <christian.gnaw@gmail.com>
2018-06-17 21:15:15 -0400

    Added create-react-app init

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+const config = {
+    apiKey: "AIzaSyDR-wc6nYAd7QE9ICLlVSfaaedsgsyLu1w",
+    authDomain: "amine-7eb29.firebaseapp.com",
+    databaseURL: "https://amine-7eb29.firebaseio.com",
+    projectId: "amine-7eb29",
+    storageBucket: "amine-7eb29.appspot.com",
+    messagingSenderId: "376873008214"
+};
+firebase.initializeApp(config);
+export default firebase;
